c50a33352e42e6d52f89c8d6d66a1b25c7bfa95e
ivan.hernandez <ivan.hernandez@dautom.com.co>
2022-11-20 12:07:05 -0500

    16. Poniendo a prueba el manejo de excepciones.

--- debugging.py
+++ debugging.py
@@ -1,15 +1,25 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
-
+    #divisors = []
+    #for i in range(1, num + 1):
+    #    if num % i == 0:
+    #        divisors.append(i)
+    try:
+        if num < 0:
+            raise ValueError('Debes ingrear un número negativo')
+        divisors = [i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
+        
 
 def run():
-    num = int(input('Ingresa un número: '))
-    print(divisors(num))
-    print('Termino la ejecución del programa')
+    try:
+        num = int(input('Ingresa un número: '))
+        print(divisors(num))
+        print('Termino la ejecución del programa')
+    except:
+        print('Debes ingrear un número.')
 
 
 if __name__ == '__main__':
